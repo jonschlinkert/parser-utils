7165128641b4f891a42e3eb3b5943dfe440f3beb
jonschlinkert <dev@sellside.com>
2014-08-26 19:58:49 -0400

    update utils to more consistently normalize file objects

--- index.js
+++ index.js
@@ -9,6 +9,26 @@
 
 var _ = require('lodash');
 
+
+/**
+ * Default properties expected on `file` objects.
+ *
+ *   - `path` The source file path.
+ *   - `data`: metadata, like from yaml front matter
+ *   - `contetn`: the content of a file, excluding front-matter
+ *
+ * @return {Array}
+ * @api private
+ */
+
+exports.file = {
+  path: '',
+  content: '',
+  orig: {},
+  data: {}
+};
+
+
 /**
  * Properties to be applied to `file` objects.
  *
@@ -26,66 +46,109 @@ exports.dataProps = [
 
 
 /**
- * Return a single  object from `locals` and `data`
- * properties on the given object.
+ * Return an object composed only of `data` properties.
+ * If a `locals` object is supplied, properties in that
+ * object will override any properties on the `data`
+ * object.
  *
- * @param {Object} `obj` The object with properties to merge.
- * @param {Object} `props` You may pass an array of additional properties to merge.
- * @return {Object} Merged object
- * @api public
+ * @param  {Object} `obj` Object with data objects to merge.
+ * @param  {Object} `locals` Optional object of data that should "win" over other data.
+ * @param  {Array} `props` Additional properties to merge in.
+ * @return {Object} Object composed only of a single data property.
+ * @api private
  */
 
-exports.mergeData = function (obj, props) {
+exports.mergeData = function (obj, locals, props) {
+  if (Array.isArray(locals)) {
+    props = locals;
+    locals = {};
+  }
+
   props = _.pick(obj, props || exports.dataProps);
-  var data = [].concat(_.values(props));
+  var data = _.values(props).concat(locals);
+
   return _.merge.apply(_, data);
 };
 
 
 /**
+ * Return an object composed only of `data` properties.
+ *
+ * @return {Object}
+ * @api private
+ */
+
+exports.siftKeys = function (obj, props) {
+  obj = _.merge({}, exports.file, obj);
+
+  var file = _.keys(exports.file);
+  var keys = _.keys(obj);
+  var diff = _.difference(keys, file);
+
+  var o = _.pick(obj, file);
+
+  o.orig = _.merge({}, o.orig, _.pick(obj, diff));
+  defineGetter(o.orig, 'content', function(value) {
+    return o.content;
+  });
+
+  return o;
+};
+
+
+/**
  * Extend and normalize a file object, to ensure that it
- * has the properties expected by the next parser.
+ * has the _ideal_ properties expected by the next parser.
  *
  * **Example:**
  *
  * ```js
  * var file = {content: 'foo'};
  * utils.extendFile(file);
- * //=> {content: 'foo', data: {}, original: 'foo', options: {}}
+ * //=> {content: 'foo', data: {}, orig: 'foo', options: {}}
  * ```
  *
- * @param  {Object} `file` The file object or string to normalize.
- * @param  {Object} `options` Options with additional properties to extend to the file object.
- * @return {Object} Normalized file object.
- * @api public
+ * @param  {Object} `file`
+ * @param  {Object} `options`
+ * @return {Object}
  */
 
 exports.extendFile = function extendFile(file, options) {
   var opts = _.extend({}, options);
+  var o = {};
 
-  // Properties to merge onto the file object.
-  var o = {data: {}, options: {}, content: '', original: ''};
-  var props = _.keys(_.clone(o));
-
-  // Normalize `file.content`
-  if (typeof file === 'string') {
-    o.original = file;
-    o.content = file;
-  } else {
-    file = file || {};
-    var original = file.original;
-    _.merge(opts, file.options);
-    _.merge(o, file);
+  if (!file) {
+    return exports.file;
   }
 
-  // Merge data
-  var obj = _.merge({}, o, file, opts);
-  o.data = exports.mergeData(obj);
+  if (typeof file === 'string') {
+    o.content = file;
+  } else {
+    o = file;
+  }
 
-  // Move non-file props to the options
-  o.options = _.omit(obj, props);
+  // gray-matter compatibility
+  if (o.original) {
+    o.orig = o.original;
+    delete o.original;
+  }
 
-  // Move non-file props to the options
-  o = _.pick(o, props);
-  return o;
+  var locals = opts;
+  if (opts.hasOwnProperty('locals')) {
+    locals = opts.locals;
+  }
+
+  o.data = exports.mergeData(o, locals);
+  return exports.siftKeys(o)
+};
+
+
+
+function defineGetter(obj, name, getter) {
+  Object.defineProperty(obj, name, {
+    configurable: false,
+    enumerable: true,
+    get: getter,
+    set: function() {}
+  });
 };
