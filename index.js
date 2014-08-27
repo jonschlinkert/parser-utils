81e989b1964e0e22564db2986e8a755f496465d8
jonschlinkert <dev@sellside.com>
2014-08-27 01:47:57 -0400

    v0.2.0

--- index.js
+++ index.js
@@ -11,17 +11,45 @@ var _ = require('lodash');
 
 
 /**
+ * Expose `utils`
+ */
+
+var utils = module.exports;
+
+
+/**
+ * Utility method to define getters.
+ *
+ * @param  {Object} `obj`
+ * @param  {String} `name`
+ * @param  {Function} `getter`
+ * @return {Getter}
+ * @api private
+ */
+
+function defineGetter(obj, name, getter) {
+  Object.defineProperty(obj, name, {
+    configurable: false,
+    enumerable: true,
+    get: getter,
+    set: function() {}
+  });
+}
+
+
+/**
  * Default properties expected on `file` objects.
  *
  *   - `path` The source file path.
  *   - `data`: metadata, like from yaml front matter
- *   - `contetn`: the content of a file, excluding front-matter
+ *   - `content`: the content of a file, **excluding** front-matter
+ *   - `orig`: the original content of a file, **including** front-matter
  *
  * @return {Array}
  * @api private
  */
 
-exports.file = {
+utils.file = {
   path: '',
   content: '',
   orig: {},
@@ -39,56 +67,70 @@ exports.file = {
  * @api private
  */
 
-exports.dataProps = [
+utils.dataProps = [
   'locals',
   'data'
 ];
 
 
 /**
- * Return an object composed only of `data` properties.
- * If a `locals` object is supplied, properties in that
- * object will override any properties on the `data`
- * object.
+ * Get the array of keys expected on normalized `file` objects.
  *
- * @param  {Object} `obj` Object with data objects to merge.
- * @param  {Object} `locals` Optional object of data that should "win" over other data.
- * @param  {Array} `props` Additional properties to merge in.
- * @return {Object} Object composed only of a single data property.
- * @api private
+ * @return {Array} Array of keys.
+ * @api public
  */
 
-exports.mergeData = function (obj, locals, props) {
-  if (Array.isArray(locals)) {
-    props = locals;
-    locals = {};
+utils.fileKeys = Object.keys(utils.file);
+
+
+/**
+ * Get an array of keys that should not be on the root of a
+ * normalized `file` object.
+ *
+ * **Example:**
+ *
+ * ```js
+ * var utils = require('parser-utils');
+ * var file = {a: 'a', b: 'b', path: 'a/b/c.md'};
+ * console.log(utils.diffKeys(file));
+ * //=> ['a', 'b']
+ * ```
+ *
+ * @param {Object} `obj` The object to inspect.
+ * @param {Array} `props` Array of properties to concat to the output.
+ * @return {Array} Array of keys
+ * @api public
+ */
+
+utils.diffKeys = function diffKeys(o, props) {
+  if (!props) {
+    props = [];
   }
 
-  props = _.pick(obj, props || exports.dataProps);
-  var data = _.values(props).concat(locals);
+  var keys = Object.keys(o).concat(props);
+  var file = utils.fileKeys;
 
-  return _.merge.apply(_, data);
+  return _.difference(keys, file);
 };
 
 
 /**
- * Return an object composed only of `data` properties.
+ * Normalize the properties on the given `obj`.
  *
- * @return {Object}
- * @api private
+ * @param  {Object} `obj` The object to normalize.
+ * @param  {Array} `props` Any additional properties to include.
+ * @return {Object} Normalized object.
+ * @api public
  */
 
-exports.siftKeys = function (obj, props) {
-  obj = _.merge({}, exports.file, obj);
+utils.siftKeys = function siftKeys(obj, props) {
+  obj = _.merge({}, utils.file, obj);
 
-  var file = _.keys(exports.file);
-  var keys = _.keys(obj);
-  var diff = _.difference(keys, file);
-
-  var o = _.pick(obj, file);
+  var diff = utils.diffKeys(obj, props);
+  var o = _.pick(obj, utils.fileKeys);
 
   o.orig = _.merge({}, o.orig, _.pick(obj, diff));
-  defineGetter(o.orig, 'content', function(value) {
+  defineGetter(o.orig, 'content', function() {
     return o.content;
   });
 
@@ -97,28 +139,77 @@ exports.siftKeys = function (obj, props) {
 
 
 /**
+ * Return an object composed only of `data` properties. If a `locals` object
+ * is supplied, properties in that object will override any properties on the
+ * `data` object. If a `locals` object is defined, and/or if the `locals` object
+ * has a nested `locals` property, both will be merged with the `data` property
+ * on the returned object.
+ *
+ * @param  {Object} `obj` Object with data objects to merge.
+ * @param  {Object} `locals` Optional object of data that should "win" over other data.
+ * @param  {Array} `props` Additional properties to merge in.
+ * @return {Object} Object with a single `data` property.
+ * @api public
+ */
+
+utils.mergeData = function mergeData(obj, locals, props) {
+  if (Array.isArray(locals)) {
+    props = locals;
+    locals = {};
+  }
+
+  props = _.pick(obj, props || utils.dataProps);
+  var data = _.values(props).concat(locals);
+
+  return _.merge.apply(_, data);
+};
+
+
+/**
+ * Merge nested properties into the root of the given object. Nested
+ * properties occur when, for instance, and options object - intended
+ * as `locals`, is also passed with an actual `locals` property.
+ *
+ * **Note** that this will only merge properties one level deep.
+ *
+ * @param  {Object} `obj` The object to flatten.
+ * @param  {String} `key` The property to merge onto the root of the object.
+ * @return {Object} Object with `locals` merged into the root.
+ * @api public
+ */
+
+utils.flattenObject = function flattenObject(o, key) {
+  if (o.hasOwnProperty(key)) {
+    o = _.merge(o, o[key]);
+    delete o[key];
+  }
+  return o;
+};
+
+
+/**
  * Extend and normalize a file object, to ensure that it
- * has the _ideal_ properties expected by the next parser.
+ * has the properties expected by the next parser.
  *
  * **Example:**
  *
  * ```js
  * var file = {content: 'foo'};
  * utils.extendFile(file);
- * //=> {content: 'foo', data: {}, orig: 'foo', options: {}}
+ * //=> {path: '', content: 'foo', data: {}, orig: 'foo'}
  * ```
  *
- * @param  {Object} `file`
- * @param  {Object} `options`
- * @return {Object}
+ * @param  {Object|String} `file` The value to normalize.
+ * @param  {Object} `options` Options or locals.
+ * @return {Object} File object with normalized properties.
  */
 
-exports.extendFile = function extendFile(file, options) {
+utils.extendFile = function extendFile(file, options) {
   var opts = _.extend({}, options);
   var o = {};
 
   if (!file) {
-    return exports.file;
+    return utils.file;
   }
 
   if (typeof file === 'string') {
@@ -127,28 +218,18 @@ exports.extendFile = function extendFile(file, options) {
     o = file;
   }
 
-  // gray-matter compatibility
   if (o.original) {
     o.orig = o.original;
     delete o.original;
   }
 
-  var locals = opts;
-  if (opts.hasOwnProperty('locals')) {
-    locals = opts.locals;
-  }
+  var locals = utils.flattenObject(opts, 'locals');
 
-  o.data = exports.mergeData(o, locals);
-  return exports.siftKeys(o)
+  o.data = utils.mergeData(o, locals);
+  return utils.siftKeys(o);
 };
 
 
 
-function defineGetter(obj, name, getter) {
-  Object.defineProperty(obj, name, {
-    configurable: false,
-    enumerable: true,
-    get: getter,
-    set: function() {}
-  });
-};
+
+module.exports = utils;
